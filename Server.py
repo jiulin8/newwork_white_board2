289a75a3094abfc0c1772fbf608c23d430e6f528
zhangsan <zhangsan@qq.com>
2020-01-06 11:04:46 +0800

    basicUserLogin

--- Server.py
+++ Server.py
@@ -1,3 +1,29 @@
-print('hello')
-print('aaaa')
-print('sssds')
+import socket
+import threading
+import time
+
+
+class Server:
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+
+        self.network = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.network.bind((self.host,self.port))
+        self.network.listen(20)
+
+        print(f'server listen at {self.port}')
+
+    def start(self):
+        while True:
+            client_sock,client_addr = self.network.accept()
+            print(f'client {client_addr} connected')
+            time.sleep(0.1)
+            threading.Thread(target=self.wait_for_user_nickname,args=[client_sock])
+            print('sss')
+    def wait_for_user_nickname(self,client_sock):
+        print('aaa')
+        print(self.client_sock)
+if __name__ == '__main__':
+    server = Server('0.0.0.0',6000)
+    server.start()
